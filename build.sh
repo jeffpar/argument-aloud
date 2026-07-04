19a53c756aecf6a42a17b286c196af6552cc7610
Jeff Parsons <Jeff@pcjs.org>
2026-07-03 21:14:02 -0700

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
+set -e
+
 node scripts/update_cases.js
-rsync -tr --exclude=".*" courts/ussc/indexes/ ../argument-apart/courts/ussc/indexes/
+rsync -r --exclude=".*" courts/ussc/indexes/ ../argument-apart/courts/ussc/indexes/
+
+if [ -n "$1" ]; then
+  MSG="$1"
+
+  sync_to_website() {
+    git add -A
+    git diff --cached --quiet || git commit -m "$MSG"
+    git push
+    git checkout website
+    git merge main --no-edit
+    git push
+    git checkout main
+  }
+
+  sync_to_website
+
+  pushd ../argument-apart > /dev/null
+  sync_to_website
+  popd > /dev/null
+fi
